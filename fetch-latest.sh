372455f2dff5bb30ca88c91b388e36035207379a
John Stultz <john.stultz@linaro.org>
2018-03-05 15:38:15 -0800

    fetch-latest.sh: Update build snapshot URLs and add some output during downloads
    Update the scripts to use the new build snapshot URLs
    and add additional output during downloads as it might
    take awhile and users probably would like some feedback.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>

--- fetch-latest.sh
+++ fetch-latest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SERVER="https://builds.96boards.org"
+SERVER="https://snapshots.linaro.org"
 FILE_LIST="boot.img.xz cache.img.xz userdata.img.xz"
 LICENCED_FILE_LIST="system.img.xz"
 BOARDS_LIST="hikey hikey960"
@@ -14,7 +14,7 @@ for board in $BOARDS_LIST; do
   rm -f $FILE_LIST
 
   # This is a bit gross, but basically grep through the index.html to find the latest numbered snapshot dir
-  LATEST=`wget -O - "$SERVER/snapshots/$board/linaro/aosp-master/" 2>/dev/null | grep "snapshots/$board/linaro/aosp-master/" | head -2 | tail -1`
+  LATEST=`wget -O - "$SERVER/96boards/$board/linaro/aosp-master//" 2>/dev/null | grep "96boards/$board/linaro/aosp-master/" | head -2 | tail -1`
   LATEST=`echo $LATEST | sed -e 's/<a href=//' | sed -e 's/^[ \t]*//' |  sed -e 's/"//g'`
   LATEST_URL="$SERVER$LATEST"
 
@@ -22,11 +22,13 @@ for board in $BOARDS_LIST; do
   echo "$LATEST_URL" > build-url.txt
 
   for i in $FILE_LIST; do
+    echo wget "$LATEST_URL/$i"
     wget "$LATEST_URL/$i" 2> /dev/null
   done
   for i in $LICENCED_FILE_LIST; do
      # thanks to vishal for this trick!
-     curl --fail --silent -L --show-error -b license_accepted_eee6ac0e05136eb58db516d8c9c80d6b=yes $LATEST_URL/$i > $i
+     echo curl --fail --silent -L --show-error -b license_accepted_eee6ac0e05136eb58db516d8c9c80d6b=yes $LATEST_URL/$i 
+     curl --fail -L --show-error -b license_accepted_eee6ac0e05136eb58db516d8c9c80d6b=yes $LATEST_URL/$i -o  $i
   done
 
   cd ..
